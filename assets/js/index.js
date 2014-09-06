8e7ce75b5186db28169777d8c9d2e59913561fce
Justin Chan <justin.the.c@gmail.com>
2014-09-05 22:55:45 -0400

    Disabled skrollr on screen widths of <768px

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,23 +14,7 @@ function init() {
 	    scrollBy(0, -offset+1);
 	});
 
-
-	// Parallax Settings
-	// Init Skrollr
-	var s = skrollr.init({
-		forceHeight: false
-	});
-	// Get window size
-    var winH = $(window).height();
-    // Keep minimum height 550
-    if(winH <= 550) {
-		winH = 550;
-	}
-    // Resize our slides
-    $('.section-img-container').height(winH);
-    // Refresh Skrollr after resizing our sections
-    s.refresh($('.section-img-container'));
-
+	adjustWindow();
 
     // Navbar Scroll Detection
     $(window).scroll(function(event){
@@ -53,4 +37,48 @@ function init() {
 	
 }
 
+function adjustWindow(){
+	// Parallax Settings
+
+	// Init Skrollr for screens 768px wide and up
+	// Get Window Dimensions
+	var winH = $(window).height();
+	var winW = $(window).width();
+
+	// Keep minimum height 550
+    if(winH <= 550) {
+		winH = 550;
+	}
+
+	if(winW>=768){
+		var s = skrollr.init({
+			forceHeight: false
+		});
+	    
+	    // Resize our slides
+	    $('.section-img-container').height(winH);
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.section-img-container'));
+	}
+	else{
+        var s = skrollr.init();
+        s.destroy();
+        // Resize our slides
+	    $('.section-img-container').height(winH);
+	}
+}
+/*
+function initAdjustWindow(){
+	return {
+        match : function() {
+            adjustWindow();
+        },
+        unmatch : function() {
+            adjustWindow();
+        }
+    };
+}
+
+enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
+*/
 $(document).on('ready', init);
